1c6993bb6cd00e380e3f2894a4b1d35d80d76eab
tonyquig <tquigley9@gmail.com>
2017-03-28 10:44:31 +0100

    City selection test fix

--- app/citySelection/citySelection_test.js
+++ app/citySelection/citySelection_test.js
@@ -2,7 +2,7 @@
 
 describe('City Selection module', function () {
 
-    beforeEach(module('pyp.citySelection'));
+    beforeEach(module('pyp.citySelection', [['ngRoute', 'firebase', 'ngMap']]));
 
     describe('City selection controller', function () {
 
