0f66edf1a33027bda3de8ebb9cba8dc1a82e4ec4
tonyquig <tquigley9@gmail.com>
2017-03-28 14:08:09 +0100

    City selection test

--- app/citySelection/citySelection_test.js
+++ app/citySelection/citySelection_test.js
@@ -1,16 +1,9 @@
 'use strict';
 
 describe('City Selection module', function () {
-
-    beforeEach(module('pyp.citySelection', ['ngRoute', 'firebase', 'ngMap']));
-
-    describe('City selection controller', function () {
-
-        it('should be defined', inject(function () {
-            //spec body
-            var test = "test";
-            expect(test).toEqual("test");
-        }));
-
+    it('should add two numbers together', function () {
+        var total = 2 + 2;
+        expect(total).toEqual(4);
     });
+
 });
