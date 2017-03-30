f63b9ac494f2404371839fdb3b64bfb64cb086b3
tonyquig <tquigley9@gmail.com>
2017-03-30 15:10:43 +0100

    Rating page 5 star review implemented

--- app/rating/rating.js
+++ app/rating/rating.js
@@ -15,4 +15,42 @@ angular.module('pyp.rating', ['ngRoute', 'firebase', 'jkAngularRatingStars'])
     console.log('Selected bars: ', $scope.selectedBars);
 
     console.log('$scope.barName: ', $scope.barName);
+
+    $scope.priceRating = 3;
+    $scope.foodRating = 3;
+    $scope.drinkRating = 3;
+    $scope.atmosphereRating = 3;
+    $scope.ageRating = 3;
+    $scope.dressRating = 3;
+
+    $scope.onPriceRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
+    $scope.onFoodRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
+    $scope.onDrinkRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
+    $scope.onAtmosphereRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
+    $scope.onAgeRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
+    $scope.onDressRating = function (rating, name) {
+        console.log("name: ", name);
+        console.log("Rating: ", rating);
+    }
+
 }]);
